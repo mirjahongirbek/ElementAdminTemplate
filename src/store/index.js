b9dc528273e373cb5eabca3edf2b22a040aa21d8
joha1 <mirjahongirbek@gmail.com>
2020-04-15 10:56:14 +0500

    delete components

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
 Vue.use(Vuex)
-
 const modulesFiles = require.context('./modules', true, /\.js$/)
-
-
-
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   // set './app.js' => 'app'
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
@@ -19,7 +15,6 @@ const store = new Vuex.Store({
   modules,
   getters,
   state: {
-    
   }
 })
 
